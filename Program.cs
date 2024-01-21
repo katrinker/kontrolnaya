921f82e25d0d495b3e2c5232db2f479b1d9dddc9
Catherine <katrinker@bk.ru>
2024-01-21 21:36:43 +0700

    create method for count symbols and create new array

--- Program.cs
+++ Program.cs
@@ -23,3 +23,21 @@ for(int i = 0; i < array1.Length; i++)
 
 string[] array2 = new string[array1.Length];
 
+NewArray(array1, array2);
+PrintArray(array2);
+
+void NewArray (string[] array1, string[] array2)
+{
+ int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+
+
